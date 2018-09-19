eb86faee46e3bb5a5778af5f9d92e63d74335ea0
Ibrahim Tchee <ibrahim.chechen64@gmail.com>
2018-09-19 22:25:22 +0200

    Single item page nearly done and cart page created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $('.sidenav').sidenav({ edge: 'right' });
+    $('.tabs').tabs();
+    $('.modal').modal();
+
     $(window).scroll(function(){
         if ($(window).scrollTop() < 2)
         {
@@ -8,6 +12,31 @@ $(document).ready(function(){
         }
     });
 
-    $('.sidenav').sidenav({ edge: 'right' });
-    $('.tabs').tabs();
+
+    $('.quantity.input button.inc').click(function(){
+        quantity = increaseQuantity($('.quantity.input .quantity.count').data('quantity'));
+
+        $('.quantity.input .quantity.count').data('quantity', quantity);
+        $('.quantity.input .quantity.count').html(quantity);
+    });
+
+    $('.quantity.input button.dec').click(function(){
+        quantity = decreaseQuantity($('.quantity.input .quantity.count').data('quantity'));
+        $('.quantity.input .quantity.count').data('quantity', quantity);
+        $('.quantity.input .quantity.count').html(quantity);
+    });
+
 });
+
+function increaseQuantity(quantity)
+{
+    return quantity + 1;
+}
+
+function decreaseQuantity(quantity)
+{
+    if (quantity <= 1){
+        return 1;
+    }
+    return quantity - 1;
+}
